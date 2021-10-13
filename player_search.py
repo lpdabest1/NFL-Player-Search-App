fc2d99e8ef59a752051beff01d1047740f3feecd
lpdabest1 <lorenzo.pesina@utexas.edu>
2021-10-12 20:46:49 -0500

    Update player_search.py

--- player_search.py
+++ player_search.py
@@ -13,7 +13,7 @@ import requests
 # Uses st.cache to only run once.
 @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
-    return mysql.connector.connect(**st.secrets["mysql"])
+    return mysql.connector.connect(st.secrets["mysql"])
     
 conn = init_connection()
 
