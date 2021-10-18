1516def2105de442fd1f43f50c6de6f0d86c62af
lpdabest1 <lorenzo.pesina@utexas.edu>
2021-10-18 17:22:36 -0500

    updates to chart

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NFL-Player_Search_App
+# NFL-Player-Search-App
 An application that allows user to search for an NFL Player and retrieve data/information about them.
--- player_search.py
+++ player_search.py
@@ -9,38 +9,16 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 import requests
-# Initialize connection.
-# Uses st.cache to only run once.
-#@st.cache(allow_output_mutation=True, suppress_st_warning=True, hash_funcs={"_thread.RLock": lambda _: None})
-#def init_connection():
-#    #return mysql.connector.connect(**st.secrets["mysql"])
-#    return mysql.connector.connect(
-#    host="localhost",
-#    user="root",
-#    port=3306,
-#    password="s2037940!!PP",
-#    database="nfl_stats")
-    
-#conn = init_connection()
 
+# Hex Codes for the NFL Teams, which will be stored
+team_colors = {'Arizona Cardinals':'#97233f', 'Atlanta Falcons':'#a71930', 'Baltimore Ravens':'#241773', 'Buffalo Bills':'#00338d', 'Carolina Panthers':'#0085ca',
+            'Chicago Bears':'#0b162a', 'Cincinnati Bengals':'#fb4f14', 'Cleveland Browns':'#311d00', 'Dallas Cowboys':'#041e42', 'Denver Broncos':'#002244', 
+            'Detroit Lions':'#0076b6', 'Green Bay Packers':'#203731', 'Houston Texans':'#03202f', 'Indianapolis Colts':'#002c5f', 'Jacksonville Jaguars':'#006778', 
+            'Kansas City Chiefs':'#e31837', 'Los Angeles Chargers':'#002a5e', 'Los Angeles Rams':'#003594', 'Miami Dolphins':'#008e97', 
+            'Minnesota Vikings':'#4f2683', 'New England Patriots':'#002244', 'New Orleans Saints':'#d3bc8d', 'New York Giants':'#0b2265', 
+            'New York Jets':'#125740', 'Las Vegas Raiders':'#000000', 'Philadelphia Eagles':'#004c54', 'Pittsburgh Steelers':'#ffb612', 
+            'San Francisco 49ers':'#aa0000', 'Seattle Seahawks':'#002244', 'Tampa Bay Buccaneers':'#d50a0a', 'Tennessee Titans':'#0c2340', 'Washington Football Team':'#773141'}
 
-
-# Perform query.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-#@st.cache(ttl=600)
-#def run_query(query):
-#    with conn.cursor() as cur:
-#        cur.execute(query)
-#        return cur.fetchall()
-
-# comments
-#rows = run_query("SELECT * FROM nfl_player_qb_search")
-#rows = run_query("SELECT * FROM nfl_qb_;")
-#player_img = run_query("SELECT * FROM nfl_qb_search_images;")
-
-# Print results.
-#for row in rows:
-    #st.write(f"{row[0]} has a :{row[1]}:")
 rows = pd.read_csv('NFL_QB_Search.csv')
 player_img = pd.read_csv('NFL_QB_Search_Images.csv')
 qb_data = pd.DataFrame(rows)
@@ -49,22 +27,6 @@ qb_img = pd.DataFrame(player_img)
 #qb_data.columns = ['Player','Image_Url','Image','Team','Age','GP','GS','Record','Wins %','Cmp','Att','Cmp%','Yds','TD','TD%','INT','INT%','1D','Lng','Y/A','AY/A','Y/C','Y/G','Rating','QBR','Sck','Sck%','Yds Loss']
 qb_data.columns = ['Player','Team','Age','GP','GS','Cmp','Att','Cmp%','Yds','TD','TD%','INT','INT%','Lng','Y/A','AY/A','Y/C','Y/G','Rating','Year']
 qb_img.columns = ['Player','Player Image','Year']
-
-
-
-#qb_data[qb_data['Player'] == 'Tom Brady']
-#st.dataframe(qb_data)
-
-#qb_data = qb_data.astype(dtype={'Player': 'string'})
-
-
-#edited_qb_data = qb_data
-
-#edited_qb_data['Player'] = qb_data['Player'].str.split(',')
-#edited_qb_data['Image_Url'] = qb_data['Image_Url'].str.split(',')
-#edited_qb_data['Team'] = qb_data['Team'].str.split(',')
-#data_types = qb_data.dtypes                   
-#print(data_types)
 
 current_season = 2021
 sorted_unique_players = sorted(qb_data.Player.unique())
@@ -75,8 +37,7 @@ years = []
 # Selecting a Player
 user_input = st.selectbox('Select a player to search', sorted_unique_players)
 
-# Grab Team of selected player
-qb_team = qb_data.loc[qb_data['Player']== user_input, ['Team','Year']]
+
 
 # Grab Active Years of Player in the NFL
 years_qb = qb_data.loc[qb_data['Player']== user_input, ['Year']]
@@ -87,14 +48,21 @@ for i in years_qb['Year']:
 # Selecting a year based on user input (selected player)
 if user_input:
     selected_years = st.selectbox('Select a Year', list(reversed(years)))
-# Two Columns (one for image and one for data)
-#col1, col2= st.columns([1,1])
-#if user_input:
-#    player_image = qb_data.loc[qb_data['Player']== user_input, ['Image_Url']]
-#    for i in player_image.Image_Url:
-#        get_url = requests.get(i)
-#        img = Image.open(BytesIO(get_url.content))
-#        col1.image(img, width=250, caption=user_input)
+
+
+# Getting Team Color (if available) based on user input and selected year
+qb_team = qb_data.loc[qb_data['Player']== user_input, ['Team','Year']]
+qb_current_team = qb_team.loc[qb_team['Year']== selected_years, ['Team']]
+Team = ''
+year_check = 0
+if user_input and selected_years:
+    for i in qb_current_team.Team:
+        Team = i
+    for j in qb_team.Year:
+        if j == selected_years:
+            year_check = j
+    #st.write(Team, year_check)
+
 
 col1, col2= st.columns([1,1])
 if user_input:
@@ -105,20 +73,12 @@ if user_input:
         col1.image(img, width=250, caption=user_input)
         break
 
-
-
-
-
-#qb_data.rename(columns={'Completion Percentage': 'Cmp%',
-#'Passing Yards': 'Pass Yds', 'Passing Touchdowns':'Pass TD','Touchdown Percentage':'TD%','Interceptions Percentage':'INT%'}, inplace= True)
-#print(df)
-
 #   Naveen Venkatesan --> Data Scientist url:https://towardsdatascience.com/scraping-nfl-stats-to-compare-quarterback-efficiencies-4989642e02fe
 #   This is where I found a template for how to generate radar charts for comparing nfl quarterbacks
 # Stat Categories
-# Cmp%, Pass Yds, Passing TDs, TD%, INT, INT%, QBR
+# Cmp%, Pass Yds, Passing TDs, TD%, INT, INT%, Rating
 
-#stat_categories = ['Completion Percentage','Passing Yards','Passing Touchdowns','Touchdown Percentage','Interceptions','QBR']
+#stat_categories = ['Completion Percentage','Passing Yards','Passing Touchdowns','Touchdown Percentage','Interceptions','Rating']
 
 # Reducing items in dataframe based on selected year
 stats_selected_year = qb_data[qb_data['Year'] ==selected_years]
@@ -165,14 +125,7 @@ mpl.rcParams['font.size'] = 16
 mpl.rcParams['axes.linewidth'] = 0
 mpl.rcParams['xtick.major.pad'] = 15
 
-# Hex Codes for the NFL Teams, which will be stored
-team_colors = {'Arizona Cardinals':'#97233f', 'Atlanta Falcons':'#a71930', 'Baltimore Ravens':'#241773', 'Buffalo Bills':'#00338d', 'Carolina Panthers':'#0085ca',
-            'Chicago Bears':'#0b162a', 'Cincinnati Bengals':'#fb4f14', 'Cleveland Browns':'#311d00', 'Dallas Cowboys':'#041e42', 'Denver Broncos':'#002244', 
-            'Detroit Lions':'#0076b6', 'Green Bay Packers':'#203731', 'Houston Texans':'#03202f', 'Indianapolis Colts':'#002c5f', 'Jacksonville Jaguars':'#006778', 
-            'Kansas City Chiefs':'#e31837', 'Los Angeles Chargers':'#002a5e', 'Los Angeles Rams':'#003594', 'Miami Dolphins':'#008e97', 
-            'Minnesota Vikings':'#4f2683', 'New England Patriots':'#002244', 'New Orleans Saints':'#d3bc8d', 'New York Giants':'#0b2265', 
-            'New York Jets':'#125740', 'Las Vegas Raiders':'#000000', 'Philadelphia Eagles':'#004c54', 'Pittsburgh Steelers':'#ffb612', 
-            'San Francisco 49ers':'#aa0000', 'Seattle Seahawks':'#002244', 'Tampa Bay Buccaneers':'#d50a0a', 'Tennessee Titans':'#0c2340', 'Washington Football Team':'#773141'}
+
 
 # Calculate angles for radar chart
 offset = np.pi/6
@@ -211,42 +164,27 @@ def get_qb_player_data(data, player):
     return np.asarray(data[data['Player'] == player])[0]
 
 
-# ******************User Input for customized Radar Chart ****************************
-# Sidebar User Options based on selected team and player
-#sorted_unique_players_ = sorted(stats_data_categories.Player.unique())
-
-
-#col3.title('Performance Chart')
-#col3.info('A Chart that visualizes how big ben did compared to his peers for the 2020 season')
-
-
-# Created Figure for Player ###########################################################################################################
-#col3.subheader('Player Search:')
-#user_input_demo_player_ = st.sidebar.selectbox('Player(s):', sorted_unique_players_)
-
-
 # Unique Player Create Figure
 fig_player = plt.figure(figsize=(8, 8), facecolor='white')# Add subplots
 ax2 = fig_player.add_subplot(222, projection='polar', facecolor='#ededed')
 data_demo_player = get_qb_player_data(stats_data_categories, user_input)# Plot QB data
 
 
-
-
-
-if user_input:
-    if st.sidebar.checkbox('Custom Color'):
-        custom_color = st.sidebar.text_input("Enter a custom color for chart")
-        st.sidebar.info('You can type in a color to customize the radar chart to your liking. Blue, Teal, Red perhaps? Just enter it to give it a try. Note: The default color is blue if text is left empty.')
-        if not team_colors:
-            default = 'blue'
-            custom_color=default
+if user_input and selected_years:
+    if Team not in team_colors:
+        custom_color='blue'
+        if st.sidebar.checkbox('Custom Color'):
+            custom_color = st.sidebar.color_picker("Pick a custom color for player chart")
+            #custom_color = st.sidebar.text_input("Enter a custom color for chart")
+            st.sidebar.info('You can type in a color to customize the radar chart to your liking. Blue, Teal, Red perhaps? Just enter it to give it a try. Note: The default color is blue if text is left empty.')
         ax2 = create_radar_chart(ax2, angles, data_demo_player, color=custom_color)
     else:
-        ax2 = create_radar_chart(ax2, angles, data_demo_player)
+        ax2 = create_radar_chart(ax2,angles,data_demo_player, team_colors[Team])
     col2.pyplot(fig_player)
     col2.write('As displayed above, the main points of emphasis that I have selected to compare for the quarterbacks in regards to the passing game are: Cmp%, Pass Yds, Passing TDs, TD%, INT, INT%, Rating. The greater the height and shape of one category, the better the player was in that category.')
-    
+
+
+
     # DataFrame for Team Passing Rankings
     #Player_Ranks_df = stats_data_categories.loc[stats_data_categories['Player']==user_input]
     Player_Ranks_df = qb_data.loc[qb_data['Player']==user_input]
@@ -261,29 +199,8 @@ if user_input:
 
 
 
-#for i in qb_data['Player']:
-#   if user_input == i:
-#        selected_player = qb_data.loc[qb_data['Player']==user_input]
-            #selected_player = qb_data[qb_data['Team'] == user_input]
-        #col2.dataframe(selected_player)
-    
-
-
     # **************** Ranking(s) **************************************
     # ['Cmp%','Yds','TD','TD%','INT','Rating']
-
-
-
-#st.title('Quarterback Rankings')
-#st.markdown("""
-#Ranking the Quarterbacks of this season based on the criteria of Completion Percentage, Passing Yards, Passing Touchdowns, Touchdown Percentage, Interception Percentage, and QBR!
-#I will use a formula that fits the criteria mentioned to rank the quarterbacks.
-#""")
-
-# Equation for rankings:
-# QB Ranking = (50)QBR + (50)Cmp% + (50)Pass Yds + (50)Pass TD + (50)TD% + (50)INT%
-
-#st.latex('QB Ranking = (50)QBR + (10)Cmp Percentage + (10)Pass Yds + (10)Pass TD + (10)Pass TD Percentage + (10)INT Percentage')
 
 if selected_years >= 1960 and selected_years < 1970:
     rankings_df = stats_data_categories.head(20)
