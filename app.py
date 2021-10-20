dd64e01fe1f1438dd96f019343204ad5f9a8ae68
lpdabest1 <lorenzo.pesina@utexas.edu>
2021-10-20 18:38:30 -0500

    updates

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import streamlit as st
-
-import quarterbacks_script
-import database_qb_2020
-#quarterbacks_script.app()
+import player_search_passing
 
 
-database_qb_2020.app()
+st.set_page_config(
+    page_title="NFL Player Search",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+st.title('Pro Football Player Search')
+st.sidebar.title('Pro Football Archives')
+
+
+Pages = {"Passers (QB)": player_search_passing
+        }
+selection = st.sidebar.selectbox("Select One Of The Following Individual Categories",list(Pages.keys()))
+page = Pages[selection]
+
+if page:
+    page.app()
+
+
+    
