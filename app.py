28b8ae77b77b35fb668904276b34482b03ec930c
Lorenzo Pesina <lorenzo.pesina@utexas.edu>
2021-10-22 03:18:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import player_search_passing
-import player_search_rushing
+import player_search_passing, player_search_rushing, player_search_receiving
+
 
 st.set_page_config(
     page_title="NFL Player Search",
@@ -12,9 +12,9 @@ st.title('Pro Football Player Search')
 st.sidebar.title('Pro Football Archives')
 
 
-
 Pages = {"Passers (QB)": player_search_passing,
-         "Rushers (RB)": player_search_rushing
+         "Rushers (RB)": player_search_rushing,
+         "Receivers (WR)": player_search_receiving
         }
 selection = st.sidebar.selectbox("Select One Of The Following Individual Categories",list(Pages.keys()))
 page = Pages[selection]
