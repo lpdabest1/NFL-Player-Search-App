ad4016636035525e15ac58a340c49b1fd70b8b5b
Lorenzo Pesina <35588088+lpdabest1@users.noreply.github.com>
2026-09-07 15:47:54 -0500

    Issue #4: update image_data assemblers for 8KB chunks

--- nfl_player_search/image_data/qb.py
+++ nfl_player_search/image_data/qb.py
@@ -1,7 +1,10 @@
 """Assemble compressed QB image CSV payload."""
-from . import qb_chunk_00, qb_chunk_01, qb_chunk_02
+from . import qb_chunk_00, qb_chunk_01, qb_chunk_02, qb_chunk_03, qb_chunk_04, qb_chunk_05
 DATA_B64 = "".join([
     qb_chunk_00.CHUNK,
     qb_chunk_01.CHUNK,
     qb_chunk_02.CHUNK,
+    qb_chunk_03.CHUNK,
+    qb_chunk_04.CHUNK,
+    qb_chunk_05.CHUNK,
 ])
--- nfl_player_search/image_data/wr.py
+++ nfl_player_search/image_data/wr.py
@@ -1,5 +1,5 @@
 """Assemble compressed WR image CSV payload."""
-from . import wr_chunk_00, wr_chunk_01, wr_chunk_02, wr_chunk_03, wr_chunk_04, wr_chunk_05, wr_chunk_06, wr_chunk_07, wr_chunk_08
+from . import wr_chunk_00, wr_chunk_01, wr_chunk_02, wr_chunk_03, wr_chunk_04, wr_chunk_05, wr_chunk_06, wr_chunk_07, wr_chunk_08, wr_chunk_09, wr_chunk_10, wr_chunk_11, wr_chunk_12, wr_chunk_13, wr_chunk_14, wr_chunk_15, wr_chunk_16, wr_chunk_17, wr_chunk_18, wr_chunk_19, wr_chunk_20
 DATA_B64 = "".join([
     wr_chunk_00.CHUNK,
     wr_chunk_01.CHUNK,
@@ -10,4 +10,16 @@ DATA_B64 = "".join([
     wr_chunk_06.CHUNK,
     wr_chunk_07.CHUNK,
     wr_chunk_08.CHUNK,
+    wr_chunk_09.CHUNK,
+    wr_chunk_10.CHUNK,
+    wr_chunk_11.CHUNK,
+    wr_chunk_12.CHUNK,
+    wr_chunk_13.CHUNK,
+    wr_chunk_14.CHUNK,
+    wr_chunk_15.CHUNK,
+    wr_chunk_16.CHUNK,
+    wr_chunk_17.CHUNK,
+    wr_chunk_18.CHUNK,
+    wr_chunk_19.CHUNK,
+    wr_chunk_20.CHUNK,
 ])
